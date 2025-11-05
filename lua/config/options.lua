82857a69e544aaaa5aec488a809476a220b740a2
bruhma <fordashton3@gmail.com>
2025-11-04 22:52:05 -0600

    update configs and add obsidian.nvim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,3 +9,9 @@ vim.opt.smartindent = true
 
 vim.g.lazyvim_python_lsp = "basedpyright"
 vim.g.lazyvim_python_ruff = "ruff"
+
+vim.g.lazyvim_picker = "fzf"
+vim.api.nvim_create_autocmd("VimLeave", {
+    pattern = "*",
+    command = "silent !zellij action switch-mode normal",
+})
