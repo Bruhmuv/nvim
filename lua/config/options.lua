30321a315f7df75964df7cd8bc8265480eba023c
bruhma <fordashton3@gmail.com>
2025-11-25 16:52:56 -0600

    snacks explorer -> neotree

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,16 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = true
+vim.opt.showtabline = 0
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.smartindent = true
 
-vim.g.lazyvim_python_lsp = "basedpyright"
 vim.g.lazyvim_python_ruff = "ruff"
 
 vim.g.lazyvim_picker = "fzf"
-vim.api.nvim_create_autocmd("VimLeave", {
-    pattern = "*",
-    command = "silent !zellij action switch-mode normal",
-})
