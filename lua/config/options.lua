9a08c936e61e2d3c6acfc9f801ecb37dea51549a
bruhmux <fordashton3@gmail.com>
2026-06-24 15:30:39 -0500

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = true
+vim.opt.scroll = 1
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
@@ -11,4 +12,4 @@ vim.opt.smartindent = true
 
 vim.g.lazyvim_python_ruff = "ruff"
 
-vim.g.lazyvim_picker = "snacks"
+vim.g.lazyvim_picker = "fzf"
